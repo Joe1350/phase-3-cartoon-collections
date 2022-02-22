d80929836762d3939dab39b8cca0dd686c940aaa
Joseph Martin <josephmartin1350@gmail.com>
2022-02-21 22:49:27 -0700

    commit tree e324b53c94534465c78ec0fb680d332102a1d4e6

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,19 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves dwarves
+  dwarves.each.with_index(1) { |dwarf, i| puts "#{i}. #{dwarf}" }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet elements
   # Your code here
+  elements.map { |element| "#{element.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls array
   # Your code here
+  array.any? { |item| item.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese array
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find { |item| cheese_types.include?(item)}
 end
